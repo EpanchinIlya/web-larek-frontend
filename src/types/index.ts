737c65f6c2160407e290c076c1517401b4d573b5
Ilya Epanchin <epanchin.ilya@yandex.ru>
2024-08-30 16:14:29 +0500

    делаю архитектуру №2

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,15 +1,17 @@
-export type method =  'online' | 'uponReceipt';
+export type Method =  'online' | 'uponReceipt' |"";
+export type Category = 'софт-скил'| 'хард-скил'	| 'другое'	| 'дополнительное'	| 'кнопка';
 
 
 
 export interface ICard {
 
     id: string;
+    title: string;
     description: string;
-    image:string;
-    title:string;
-    category:string;    
-    price:number;
+    image: string;
+    category: Category;
+    price: number;
+    isAddedToBasket: boolean;
 }
 
 
@@ -17,25 +19,48 @@ export interface ICardList{
 
  bigCard:ICard;   
  cardList:ICard[];
- initList():void;    
+ initList():void; 
+ addBigCard(card:ICard):void;
+ removeBigCard(card:ICard):void;
+
 
 }
 
-
-
-export interface IBuyer 
+export interface IBasket
 {
-    paymentMethod:method;
-    deliveryAddress:string;
-    email:string;
-    telephone:string;
+    cardListBasket:ICard[];
+    remove(id:string):void;  
+    add(id:string):void;
+    clear():void; 
+    getCardListBasket():ICard[]
+    getTotalPrise():number; 
+
+}
+
+
+export interface IContact {
+	phone: string;
+	email: string;
+}
+
+export interface IDelivery {
+	address: string;
+	payment: Method ;
+}
+
+export type IOrderAllData = Partial<IContact> & Partial<IDelivery>;
+
+
+
+export interface IOrder extends IOrderAllData  {
+	total: number;
+	items: string[];
 }
 
 
-export interface IBasket{
-
-    items:Map<string,number>;
-    remove(id:string):void;
-
+export interface IOrderResult {
+	
+	total: number;
 }
+
 
